5b44ba33b49f8cfc4db0c8fbe26222b7771d326f
arturikq <nigartur25@gmail.com>
2025-11-13 12:03:25 +0300

    zodiac plugin update

--- vscode-zodiac-plugin/src/extension.ts
+++ vscode-zodiac-plugin/src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import horoscopeData from './horoscope.json'; // импорт фразочек
+import horoscopeData from './horoscope.json';
 
 export function activate(context: vscode.ExtensionContext) {
     console.log('Плагин "Zodiac Info" вкл');
@@ -7,7 +7,6 @@ export function activate(context: vscode.ExtensionContext) {
     const signs = Object.keys(horoscopeData); 
 
     const disposable = vscode.commands.registerCommand('vscode-zodiac-plugin.showZodiac', async () => {
-        // Пользователь выбирает знак
         const zodiacRu = await vscode.window.showQuickPick(signs, {
             placeHolder: "Выберите знак зодиака"
         });
@@ -16,8 +15,6 @@ export function activate(context: vscode.ExtensionContext) {
             vscode.window.showInformationMessage("Знак не выбран");
             return;
         }
-
-        // Исправление для TypeScript: говорим, что zodiacRu точно ключ horoscopeData
         const horoscopeText = horoscopeData[zodiacRu as keyof typeof horoscopeData];
         vscode.window.showInformationMessage(`${zodiacRu}: ${horoscopeText}`);
     });
